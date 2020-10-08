0b082c92b9d87724d5183a617ab446420eb8a2bc
TheRaajput <yasharth.cse@gmail.com>
2020-10-08 12:12:00 +0530

    splashscreen

--- app/src/main/java/com/yasharth/medicai/SplashScreen.java
+++ app/src/main/java/com/yasharth/medicai/SplashScreen.java
@@ -6,28 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private LottieAnimationView splashAnim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        splashAnim = findViewById(R.id.AnimSplash);
+        LottieAnimationView splashAnim = findViewById(R.id.AnimSplash);
+        TextView companyName = findViewById(R.id.CompanyName);
+        TextView tagLine = findViewById(R.id.TagLine);
 
         Animation frontAnim = AnimationUtils.loadAnimation(this,R.anim.myanim);
 
         splashAnim.startAnimation(frontAnim);
+        companyName.startAnimation(frontAnim);
+        tagLine.startAnimation(frontAnim);
 
         final Intent i = new Intent(this,MainActivity.class);
         Thread time = new Thread()
         {
             public void run (){
                 try {
-                    sleep(5000);
+                    sleep(6500);
                 }
                 catch (InterruptedException e){
                     e.printStackTrace();
