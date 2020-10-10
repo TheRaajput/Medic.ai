dc67bd90160081963ff21db809c57f1ab78af16e
TheRaajput <yasharth.cse@gmail.com>
2020-10-11 00:09:17 +0530

    Main

--- app/src/main/java/com/yasharth/medicai/MainActivity.java
+++ app/src/main/java/com/yasharth/medicai/MainActivity.java
@@ -1,6 +1,9 @@
 package com.yasharth.medicai;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,6 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        CardView heartCard = findViewById(R.id.heartDiag);
+//        CardView lungCard = findViewById(R.id.lungDiag);
+//        CardView eyeCard = findViewById(R.id.eyeDiag);
+//        CardView healthTip = findViewById(R.id.healthTip);
+
+        heartCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toHeart = new Intent(MainActivity.this,heartActivity.class);
+                startActivity(toHeart);
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/yasharth/medicai/heartActivity.java
@@ -0,0 +1,21 @@
+package com.yasharth.medicai;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+public class heartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heart);
+
+    }
+}
