228628d2488d01c9f78231643b2259ede5f2310a
TheRaajput <yasharth.cse@gmail.com>
2020-10-12 01:23:49 +0530

    Main

--- app/src/main/java/com/yasharth/medicai/heartActivity.java
+++ app/src/main/java/com/yasharth/medicai/heartActivity.java
@@ -2,20 +2,38 @@ package com.yasharth.medicai;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.Spinner;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class heartActivity extends AppCompatActivity {
 
+    private BottomSheetBehavior bottomSheetBehavior;
+    private EditText Age, bloodPressure, cholestrol, maxHeartRate, stDepression, majorVessel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heart);
+        Button predictButton = findViewById(R.id.predictBtn);
+        LinearLayout linearLayout = findViewById(R.id.bottom_navigation_container);
 
+        bottomSheetBehavior = BottomSheetBehavior.from(linearLayout);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+        predictButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                getValues();
+            }
+        });
+    }
+
+    private void getValues() {
+        
     }
 }
