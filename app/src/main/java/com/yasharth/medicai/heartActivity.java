bd6bb23d53e22ef203b797e29f1ff66f069ed0c4
TheRaajput <yasharth.cse@gmail.com>
2020-10-18 21:51:34 +0530

    Main

--- app/src/main/java/com/yasharth/medicai/heartActivity.java
+++ app/src/main/java/com/yasharth/medicai/heartActivity.java
@@ -1,24 +1,54 @@
 package com.yasharth.medicai;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import java.lang.Object;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+import org.tensorflow.lite.Interpreter;
+
+import java.io.File;
+import java.nio.MappedByteBuffer;
 
 public class heartActivity extends AppCompatActivity {
 
     private BottomSheetBehavior bottomSheetBehavior;
-    private EditText Age, bloodPressure, cholestrol, maxHeartRate, stDepression, majorVessel;
+    private final String modelPath = "heartModel.tflite";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heart);
         Button predictButton = findViewById(R.id.predictBtn);
+
+        TextView predicted_result = findViewById(R.id.predictedScore);
+
+        EditText age = findViewById(R.id.Age);
+        EditText bloodPressure = findViewById(R.id.bloodPressure);
+        EditText cholestrol = findViewById(R.id.EnterCholestrol);
+        EditText maxHeartRate = findViewById(R.id.MaxHeartRate);
+        EditText stDepression = findViewById(R.id.StDepression);
+        EditText majorVessel = findViewById(R.id.majorVessel);
+
+        RadioGroup sex = findViewById(R.id.sex);
+        RadioGroup chestPainType = findViewById(R.id.chestPainType);
+        RadioGroup bloodSugar = findViewById(R.id.bloodSugar);
+        RadioGroup ecgReport = findViewById(R.id.ecgReport);
+        RadioGroup exercisePain = findViewById(R.id.exercisePain);
+        RadioGroup slopePeak = findViewById(R.id.slopePeak);
+        RadioGroup thalSlope = findViewById(R.id.thalSlope);
+
+
+
         LinearLayout linearLayout = findViewById(R.id.bottom_navigation_container);
 
         bottomSheetBehavior = BottomSheetBehavior.from(linearLayout);
@@ -27,13 +57,11 @@ public class heartActivity extends AppCompatActivity {
         predictButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-                getValues();
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED);
+
             }
         });
-    }
 
-    private void getValues() {
-        
     }
+    
 }
